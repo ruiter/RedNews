b2d12bc0310a62eb5f1775230e499dd6a8638d07
Ruiter Matos <ruitercomp@gmail.com>
2018-01-15 21:20:27 -0200

    creating posts layout

--- posts/src/main/java/com/ruiter/posts/list/presentation/view/PostsListActivity.kt
+++ posts/src/main/java/com/ruiter/posts/list/presentation/view/PostsListActivity.kt
@@ -1,10 +1,13 @@
 package com.ruiter.posts.list.presentation.view
 
 import android.os.Bundle
+import android.view.Menu
 import com.ruiter.posts.R
 import com.ruiter.posts.list.presentation.presenter.PostsListPresenter
 import dagger.android.support.DaggerAppCompatActivity
+import kotlinx.android.synthetic.main.posts_list_layout.*
 import javax.inject.Inject
+import android.view.MenuItem
 
 class PostsListActivity : DaggerAppCompatActivity(), PostsListView {
 
@@ -15,6 +18,9 @@ class PostsListActivity : DaggerAppCompatActivity(), PostsListView {
         super.onCreate(savedInstanceState)
 
         setContentView(R.layout.posts_list_layout)
+        setSupportActionBar(toolbar)
+
+        toolbar.title = "Red News"
 
         presenter.request()
     }
@@ -30,4 +36,13 @@ class PostsListActivity : DaggerAppCompatActivity(), PostsListView {
     override fun hideError() {
     }
 
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu, menu)
+        return true
+    }
+
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        return true
+    }
 }
